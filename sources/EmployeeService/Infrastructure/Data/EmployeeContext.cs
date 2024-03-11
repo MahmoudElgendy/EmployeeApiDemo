917df1cf5d3805a1e905476b48174714e9b1e9cd
Mahmoud Elgendy <mahmoudelgendy1@gmail.com>
2024-03-11 02:20:28 -0500

    create dbcontext

---
+++ sources/EmployeeService/Infrastructure/Data/EmployeeContext.cs
@@ -0,0 +1,40 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeeContext(EmployeeContext context)
+        {
+            _context = context;
+        }
+        public DbSet<Employee> Employee { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Employee>()
+                .Property(p => p.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>()
+           .Property(p => p.MiddleName)
+           .HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>()
+           .Property(p => p.LastName)
+           .IsRequired()
+           .HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>()
+                .Property(p => p.Salary)
+                .HasPrecision(9, 2)
+                .HasAnnotation("MaxLength", 1000000);
+
+        }
+    }
+}
